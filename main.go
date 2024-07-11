91d3b79fe1b93097ff4f574b30c009236bc9ba85
MFaisal8904 <muhammadfaisal8904@gmail.com>
2024-07-11 20:48:59 +0700

    Add main file to initialize the server and routes

--- main.go
+++ main.go
@@ -8,14 +8,11 @@ import (
 )
 
 func main() {
-	// Load configuration from .env file
 	config.LoadConfig()
 
-	// Initialize services and controllers
 	weatherService := &services.WeatherService{}
 	weatherController := &controllers.WeatherController{WeatherService: weatherService}
 
-	// Set up routes and start the server
 	router := routes.SetupRouter(weatherController)
 	router.Run(":8080")
 }
